54cf8cecbe9556dbb35822f767c1ae292c8672a0
Đặng Văn Trường <dangvantruong12@gmail.com>
2020-07-05 21:43:55 +0700

    Initial Commit

---
+++ Translator/SafariExtensionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SafariExtensionViewController.swift
+//  Translator
+//
+//  Created by Đặng Văn Trường on 7/5/20.
+//  Copyright © 2020 Đặng Văn Trường. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared: SafariExtensionViewController = {
+        let shared = SafariExtensionViewController()
+        shared.preferredContentSize = NSSize(width:320, height:240)
+        return shared
+    }()
+
+}
---
+++ TruongDV/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TruongDV
+//
+//  Created by Đặng Văn Trường on 7/5/20.
+//  Copyright © 2020 Đặng Văn Trường. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
